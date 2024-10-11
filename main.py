f9836e8165df30772e34862afd587b0fb6aa6845
Isadora Beckmann <isadora.beckmann@sou.unijui.edu.br>
2024-10-11 16:59:34 -0300

    background color

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Purple.png")
+
     run = True 
     while run:
         clock.tick(FPS)
@@ -25,8 +27,30 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
+
     pygame.quit()
     quit()
+
+def get_background(name):
+    image = pygame.image.load(join("Assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i * width, j * height]
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tuple(tile))
+
+    pygame.display.update()
+
 
 if __name__ == "__main__":
     main(window)
