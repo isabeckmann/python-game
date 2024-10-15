e5146df599d98d9c70c5806edb1c5092184a6ddb
Isadora Beckmann <isadora.beckmann@sou.unijui.edu.br>
2024-10-15 14:35:11 -0300

    gravity

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def get_background(name):
 
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 1
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -59,6 +60,7 @@ class Player(pygame.sprite.Sprite):
         self.mask = None
         self.direction = "left"
         self.animation_count = 0
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -77,7 +79,10 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count/fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1
 
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, self.rect)
